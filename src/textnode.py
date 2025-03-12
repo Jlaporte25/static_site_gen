c7cabaf7aa22263942279d6a26b15e100e123701
Jlaporte25 <jacoblaporte25@gmail.com>
2025-03-11 20:42:24 -0500

    added main and textnode files

---
+++ src/textnode.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class TextNode(Enum):
+    def __init__(text, text_type, url):
+        super().__init__(TextType)    
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __repr__():
+        return f"{self}({self.text}, {self.text_type.value()}, {self.url})"

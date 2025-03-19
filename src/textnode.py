1d467c79030afea93e31ef246e64bb160b85a8e8
Jlaporte25 <jacoblaporte25@gmail.com>
2025-03-18 22:53:33 -0500

    format

--- src/textnode.py
+++ src/textnode.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
+
 class TextNode(Enum):
     def __init__(text, text_type, url):
-        super().__init__(TextType)    
+        super().__init__(TextType)
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def __repr__():
+    def __repr__(self):
         return f"{self}({self.text}, {self.text_type.value()}, {self.url})"

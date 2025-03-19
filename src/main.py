dbed5b0e1b9f34e2283ae7f168ae48c5039f41b4
Jlaporte25 <jacoblaporte25@gmail.com>
2025-03-18 23:55:19 -0500

    working on textnode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode
+
+
+def main():
+    TextNode("This is some anchor text", "link", "https://www.boot.dev")
+
+
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -3,7 +3,6 @@ from enum import Enum
 
 class TextNode(Enum):
     def __init__(text, text_type, url):
-        super().__init__(TextType)
         self.text = text
         self.text_type = text_type
         self.url = url

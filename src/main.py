17ac8125132284e7a3679b5251dc3a7ce243b2ff
Jacob <you@example.com>
2025-03-19 23:30:26 -0500

    text node, text type and __eq__ all work as expected

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 
 
 def main():
-    TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    test_node = TextNode(
+        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
+    )
+    print(test_node)
 
 
 if __name__ == "__main__":

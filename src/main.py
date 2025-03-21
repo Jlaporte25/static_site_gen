b35996378cd0af9243b0b33af701790e62a6832b
Jlaporte25 <jacoblaporte25@gmail.com>
2025-03-21 14:06:15 -0500

    main creates htmlnode instance and print it

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from htmlnode import HTMLNode
 from textnode import TextNode, TextType
 
 
@@ -6,6 +7,11 @@ def main():
         "This is some anchor text", TextType.LINK, "https://www.boot.dev"
     )
     print(test_node)
+
+    html_node = HTMLNode(
+        "p", "this is the text inside the tag", None, {"href": "https://www.google.com"}
+    )
+    print(html_node)
 
 
 if __name__ == "__main__":

80031c215ccc25d949d1fb94951b31e46f6635d0
Jlaporte25 <jacoblaporte25@gmail.com>
2025-03-25 12:23:56 -0500

    added tests

--- src/test_split_node.py
+++ src/test_split_node.py
@@ -1,7 +1,31 @@
 import unittest
 from split_node import split_nodes_delimiter
+from textnode import TextNode, TextType
 
 
 class TestSplitNode(unittest.TestCase):
     def test_split_node_bold(self):
-        pass
+        node = TextNode("this is a string with a **bold** word", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("this is a string with a ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )
+
+    def test_split_node_bold_2(self):
+        node = TextNode("this is a **string** with a **bold** word", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("this is a ", TextType.TEXT),
+                TextNode("string", TextType.BOLD),
+                TextNode(" with a ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+        )

de11919c80a97b066cfcf0785f0476d9ada9beef
Jlaporte25 <jacoblaporte25@gmail.com>
2025-03-24 20:44:48 -0500

    refactor

--- src/main.py
+++ src/main.py
@@ -1,24 +1,10 @@
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, TextType
 from split_node import split_nodes_delimiter
+from text_to_html import text_node_to_html_node
 
 
 def main():
-    def text_node_to_html_node(self):
-        if self.text_type == TextType.TEXT:
-            return LeafNode(None, self.value, None)
-        elif self.text_type == TextType.BOLD:
-            return LeafNode("b", self.value, None)
-        elif self.text_type == TextType.ITALIC:
-            return LeafNode("i", self.value, None)
-        elif self.text_type == TextType.CODE:
-            return LeafNode("code", self.value, None)
-        elif self.text_type == TextType.LINK:
-            return LeafNode("a", self.value, {"href": f"{self.url}"})
-        elif self.text_type == TextType.IMAGE:
-            return LeafNode("img", "", {"src": f"{self.url}", "alt": f"{self.value}"})
-        else:
-            raise Exception
 
     node = TextNode("This is text with a `bold` word", TextType.TEXT)
     new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
--- src/test_main.py
+++ src/test_main.py
@@ -1,25 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from htmlnode import LeafNode
-
-
-def text_node_to_html_node(text_node):
-    if text_node.text_type == TextType.TEXT:
-        return LeafNode(None, text_node.text, None)
-    elif text_node.text_type == TextType.BOLD:
-        return LeafNode("b", text_node.text, None)
-    elif text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", text_node.text, None)
-    elif text_node.text_type == TextType.CODE:
-        return LeafNode("code", text_node.text, None)
-    elif text_node.text_type == TextType.LINK:
-        return LeafNode("a", text_node.text, {"href": f"{text_node.url}"})
-    elif text_node.text_type == TextType.IMAGE:
-        return LeafNode(
-            "img", "", {"src": f"{text_node.url}", "alt": f"{text_node.text}"}
-        )
-    else:
-        raise Exception
+from text_to_html import text_node_to_html_node
 
 
 class TestMain(unittest.TestCase):

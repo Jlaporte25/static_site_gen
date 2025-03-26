745daebbc5f65f3a2322063926763001e4d0dcc7
Jlaporte25 <jacoblaporte25@gmail.com>
2025-03-26 13:09:03 -0500

    working on markdown to blocks function, still needs to get rid of whitespace inside of each block

--- src/main.py
+++ src/main.py
@@ -6,16 +6,37 @@ from text_to_html import text_node_to_html_node
 
 def main():
 
-    node = TextNode(
-        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev) and an ![image](www.image_source.com)",
-        TextType.TEXT,
-    )
+    def text_to_textnodes(text):
+        node = TextNode(text, TextType.TEXT)
+        split_bold = split_nodes_delimiter([node], "**", TextType.BOLD)
+        split_italic = split_nodes_delimiter(split_bold, "_", TextType.ITALIC)
+        split_code = split_nodes_delimiter(split_italic, "`", TextType.CODE)
+        split_images = split_nodes_image(split_code)
+        split_final = split_nodes_link(split_images)
+        return split_final
 
-    new_nodes = split_nodes_link([node])
+    def markdown_to_blocks(markdown):
+        split_mark = markdown.split("\n\n")
+        new_list = []
+        for mark in split_mark:
+            if len(mark) > 0:
+                stripped = mark.strip()
+                final = stripped.strip(" ")
+                new_list.append(final)
 
-    final_nodes = split_nodes_image(new_nodes)
+        return new_list
 
-    print(final_nodes)
+    md = """
+    This is **bolded** paragraph
+
+    This is another paragraph with _italic_ text and `code` here
+    This is the same paragraph on a new line
+
+    - This is a list
+    - with items
+    """
+
+    print(markdown_to_blocks(md))
 
 
 if __name__ == "__main__":

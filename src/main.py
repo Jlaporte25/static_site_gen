df1d5cf7383c1fb0f7788a9ccc0fd128888b1f2d
Jlaporte25 <jacoblaporte25@gmail.com>
2025-03-30 17:28:50 -0500

    finished copy_static_to_public function

--- src/main.py
+++ src/main.py
@@ -8,20 +8,25 @@ import shutil
 
 
 def main():
-    def copy_public_dir(path):
-        if os.path.exists("public"):
-            shutil.rmtree("public")
-        os.mkdir("public")
-        dirs = os.listdir("static")
-        for dir in dirs:
-            full_path = os.path.join(dir, path)
-            if os.path.isfile(full_path):
-                shutil.copy(full_path, "public")
-            elif os.path.isdir(full_path):
-                new_path = os.path.join(full_path, path)
-                copy_public_dir(new_path)
+    def copy_static_to_public(source_dir, dest_dir):
+        # Initial cleanup
+        if os.path.exists(dest_dir):
+            shutil.rmtree(dest_dir)
+        os.mkdir(dest_dir)
 
-    copy_public_dir("static")
+        # Now copy files and directories
+        for item in os.listdir(source_dir):
+            source_item = os.path.join(source_dir, item)
+            dest_item = os.path.join(dest_dir, item)
+
+            if os.path.isfile(source_item):
+                # What should happen with files?
+                shutil.copy(source_item, dest_item)
+            elif os.path.isdir(source_item):
+                # What should happen with directories?
+                copy_static_to_public(source_item, dest_item)
+
+    copy_static_to_public("static", "public")
 
 
 if __name__ == "__main__":
